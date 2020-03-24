6015d311baa954a4e63b41e03bc4ea19175c695d
daveminh <dminh@iit.edu>
2020-03-23 21:52:40 -0500

    Some GAFF setup successes and failures

--- static_files/tutorials/3cl-pro/YANK/yaml/propagate_yaml.py
+++ static_files/tutorials/3cl-pro/YANK/yaml/propagate_yaml.py
@@ -12,8 +12,8 @@ sdf_FNs.remove(f'../ligands/0-build/{source}.sdf')
 
 for sdf_FN in sdf_FNs:
   destination = os.path.basename(sdf_FN).split('.')[0]
-  if not os.path.isfile(f'MPro_{destination}.yaml'):
-    print(f'Writing MPro_{destination}.yaml')
-    F = open(f'MPro_{destination}.yaml','w')
-    F.write(dat.replace(source, destination))
-    F.close()
+  # if not os.path.isfile(f'MPro_{destination}.yaml'):
+  print(f'Writing MPro_{destination}.yaml')
+  F = open(f'MPro_{destination}.yaml','w')
+  F.write(dat.replace(source, destination))
+  F.close()
