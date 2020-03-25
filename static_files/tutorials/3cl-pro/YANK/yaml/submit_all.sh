a999817839df5d20e0c80ce019ee3d06e9bfcae9
daveminh <dminh@iit.edu>
2020-03-24 21:46:45 -0500

    Progress on 3cl-pro demonstration

---
+++ static_files/tutorials/3cl-pro/YANK/yaml/submit_all.sh
@@ -0,0 +1,5 @@
+python submit_YANK.py --type CCB-GPU --yaml MPro_ZINC000001542916.yaml
+python submit_YANK.py --type CCB-GPU --yaml MPro_ZINC000001714738.yaml
+python submit_YANK.py --type CCB-GPU --yaml MPro_ZINC000002015152.yaml
+python submit_YANK.py --type CCB-GPU --yaml MPro_ZINC000003951740.yaml
+python submit_YANK.py --type CCB-GPU --yaml MPro_ZINC000013985228.yaml
---
+++ static_files/tutorials/3cl-pro/YANK_GAFF/yaml/submit_all.sh
@@ -0,0 +1,5 @@
+python submit_YANK.py --type CCB-CPU --yaml MPro_ZINC000001542916.yaml --setup_only 
+python submit_YANK.py --type CCB-CPU --yaml MPro_ZINC000001714738.yaml --setup_only
+python submit_YANK.py --type CCB-CPU --yaml MPro_ZINC000002015152.yaml --setup_only
+python submit_YANK.py --type CCB-CPU --yaml MPro_ZINC000003951740.yaml --setup_only
+python submit_YANK.py --type CCB-CPU --yaml MPro_ZINC000013985228.yaml --setup_only
