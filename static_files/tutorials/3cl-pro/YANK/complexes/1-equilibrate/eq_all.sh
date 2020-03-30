9e9cc5473e657db091ea22e65bf95b09bc6c4aeb
daveminh <dminh@iit.edu>
2020-03-30 10:31:31 -0500

    Week 11

--- static_files/tutorials/3cl-pro/YANK/complexes/1-equilibrate/eq_all.sh
+++ static_files/tutorials/3cl-pro/YANK/complexes/1-equilibrate/eq_all.sh
@@ -1,5 +1,5 @@
 python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000001542916_complex
-python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000001714738_complex
-python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000002015152_complex
-python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000003951740_complex
-python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000013985228_complex
+# python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000001714738_complex
+# python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000002015152_complex
+# python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000003951740_complex
+# python ./submit_simulation.py --type shared --simulation_time 5 --system_name ../0-build/ZINC000013985228_complex
