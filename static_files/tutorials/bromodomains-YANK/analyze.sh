9016087ab6ba86614af6eea6bf9ad1dbd7b9fcc9
daveminh <dminh@iit.edu>
2020-04-08 17:03:43 -0500

    Binding pose prediction

--- static_files/tutorials/bromodomains-YANK/analyze.sh
+++ static_files/tutorials/bromodomains-YANK/analyze.sh
@@ -1,2 +1,3 @@
 source /home/bxie4/local/module/miniconda3.sh
+yank analyze --store=/home/bxie4/restraint_simulation/YANK/TEST_SYSTEMS/bromodomain/3mxf/40flat_output_repeat1/experiments 
 yank analyze report --store=/home/bxie4/restraint_simulation/YANK/TEST_SYSTEMS/bromodomain/3mxf/40flat_output_repeat1/experiments --output=3mxf.ipynb
